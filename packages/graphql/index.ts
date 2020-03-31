72c43f30aa4e42f40b1fd4a3e6e73a06ce907968
Adriano Rodrigues <adrianoftz1999@gmail.com>
2020-03-31 16:21:16 -0300

    port env

--- packages/graphql/index.ts
+++ packages/graphql/index.ts
@@ -13,6 +13,7 @@ const server = new ApolloServer({
 	playground: { version: '1.7.20' },
 	introspection: true
 });
-server.listen().then(({ url }) => {
+const PORT = process.env.PORT || 4000;
+server.listen(PORT).then(({ url }) => {
 	console.log(`🚀  Server ready at ${url}`);
 });
